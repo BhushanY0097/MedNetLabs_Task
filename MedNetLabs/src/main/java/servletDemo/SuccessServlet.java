d177305155f9a6c25116c0d46a8dca3e6937f814
BhushanY0097 <108522105+BhushanY0097@users.noreply.github.com>
2022-12-10 08:36:03 +0530

    Update SuccessServlet.java

--- MedNetLabs/src/main/java/servletDemo/SuccessServlet.java
+++ MedNetLabs/src/main/java/servletDemo/SuccessServlet.java
@@ -26,5 +26,24 @@ public class SuccessServlet extends HttpServlet {
 		response.setContentType("text/html;charset=ISO-8859-1");
 		String uid = request.getParameter("uid");
 		//out.println("<h2>login is successful</h2>");
+		try {
+			List<Employee> emps = new ArrayList<Employee>();
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM employees");
+			while (rs.next()) {
+				if (!uid.equals(rs.getString(2))) {
+					Employee emp = new Employee(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4),
+							rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9),
+							rs.getString(10), rs.getString(11));
+					emps.add(emp);
+				}
+			}
+			out.println("<br><br><h2>List of registered Users: </h2>");
+			for (Employee employee : emps) {
+				out.println("<h3>" + employee.getFname() + " " + employee.getLname() + "</h3>");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
