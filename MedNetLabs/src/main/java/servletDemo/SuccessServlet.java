289252b63ad97d9e497337107c634e92d45f5d3a
BhushanY0097 <108522105+BhushanY0097@users.noreply.github.com>
2022-12-10 08:37:35 +0530

    Update SuccessServlet.java

--- MedNetLabs/src/main/java/servletDemo/SuccessServlet.java
+++ MedNetLabs/src/main/java/servletDemo/SuccessServlet.java
@@ -14,6 +14,7 @@ public class SuccessServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	RequestDispatcher reqDis = null;
+	Connection con;
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);
